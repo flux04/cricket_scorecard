a08b8653b6bd9e75d8cf811399e7acda4a794e84
flux04 <43069253+flux04@users.noreply.github.com>
2022-07-30 15:28:57 +0530

    Update scoreboard.h

--- scoreboard.h
+++ scoreboard.h
@@ -1,9 +1,13 @@
+/*header file for the program
+here we define all include statements, libraries, one constant for max line length (not used), structures and declaring functions
+this file is used for pre-processing for the entire program
+we have 6 program files for this program right now!
+*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#define MAX_LINE 30
-
+//removed all global variables for optimization
 
 typedef struct node
 {
@@ -12,6 +16,13 @@ typedef struct node
 	float str;
 	struct node *next;
 }node;
+
+struct login                           
+{
+	char username[30];
+	char password[20];
+};
+
 struct batsman
 {
    char name[25], lastname[20];
@@ -48,4 +59,4 @@ struct node* readLinkedList(char filename[]);
 void writeLinkedList(char filename[], struct node* head);
 struct node* insertAtBegin(struct node *head, char *name, char *lastname, int runs, int balls, int fours, int sixes);
 void displayLinkedList(struct node *root);
-void editNode(struct node* head, int index);
+struct node* editNode(node* head, int index);
