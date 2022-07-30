d39b67f2f9c41a67e13ba9b84a54d5d4e78b41ac
flux04 <43069253+flux04@users.noreply.github.com>
2022-07-30 15:29:22 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include "scoreboard.h"
 #include <stdio.h>
 
+/*
+main controller function
+leads flow of control to loginController as we cannot allow any operation without login
+loginController has further functions called!
+*/
 void main(){
 loginController();
 }
